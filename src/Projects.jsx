dc8f2970910e4b65a466f6549864e0d3d62609ee
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2025-03-03 16:28:41 +1000

    project layout template

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -10,21 +10,27 @@ function Projects() {
             </div>
             <div className='projectsSection'>
                 <div className='projectContent'>
-                    <div className='projectCard1'>
-                        <div className="projectCard1a">
-                            <div className='projectName'>EdScore</div>
-                            <div className='projectTech'> This is going to be my tech stack</div>
-                            <div className='projectDescription'>This is going to be a description of the project</div>
+                    <div className='projectCardFirst'>
+                        <div className="projectCarda">
+                            <div className='projectName'> EdScore </div>
+                            <div className='projectTech'> This is going to be my tech stack </div>
+                            <div className='projectDescription'> Lorem ipsum odor amet, consectetuer adipiscing elit. Ante posuere ac faucibus quis consequat vel etiam. Pharetra per mauris ac lacinia ridiculus himenaeos euismod. Donec et nisi venenatis quis curae fames. Per risus ac malesuada amet vel ultrices morbi nunc malesuada. Fringilla hendrerit faucibus ante ullamcorper; gravida congue. </div>
                         </div>
-                        <div className='projectCard1b'>
+                        <div className='projectCar1b'>
 
                         </div>
                     </div>
-                    {/* <div className='projectCard2'>
-                        <div className='projectName'>EdScore</div>
-                        <div className='projectTech'></div>
-                        <div className='projectDescr'></div>
-                    </div> */}
+
+                    <div className='projectCard'>
+                        <div className='projectCardb'>
+
+                        </div>
+                        <div className='projectCarda'>
+                            <div className='projectName'> Portfolio V1</div>
+                            <div className='projectTech'> This is going to be my tech stack </div>
+                            <div className='projectDescription'> Lorem ipsum odor amet, consectetuer adipiscing elit. Ante posuere ac faucibus quis consequat vel etiam. Pharetra per mauris ac lacinia ridiculus himenaeos euismod. Donec et nisi venenatis quis curae fames. Per risus ac malesuada amet vel ultrices morbi nunc malesuada. Fringilla hendrerit faucibus ante ullamcorper; gravida congue. </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
@@ -32,3 +38,5 @@ function Projects() {
 }
 
 export default Projects;
+
+// TODO: Animations from side when scrolling or page loads for items on page
