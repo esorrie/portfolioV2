b69eeec4398218d22a59e48d2c26a564ead9b904
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2025-04-22 16:59:10 +0100

    created a current project section

---
+++ src/Current.jsx
@@ -0,0 +1,40 @@
+import './Current.css'
+
+function Current() {
+
+    return (
+        <>
+        <div className='currentTitle'>
+            Current Work
+        </div>
+        <div className='currentSection'>
+            <div className='currentContent'>
+                <div className='currentCard'> 
+                    <div className='workTitleFirst'>
+                        Finance Tracker
+                    </div>
+                    <div className='workTech'>
+                        Tech Stack: React, Flask, PostgreSQL, Docker
+                    </div>
+                    <div className='workDescription'>
+                        Desc: Lorem ipsum odor amet, consectetuer adipiscing elit. Ante posuere ac faucibus quis consequat vel etiam. Pharetra per mauris ac lacinia ridiculus himenaeos euismod. Donec et nisi venenatis quis curae fames. Per risus ac malesuada amet vel ultrices morbi nunc malesuada. Fringilla hendrerit faucibus ante ullamcorper; gravida congue.
+                    </div>
+                </div>
+                <div className='currentCard'> 
+                    <div className='workTitle'>
+                        Portfolio V2
+                    </div>
+                    <div className='workTech'>
+                        Tech Stack: React
+                    </div>
+                    <div className='workDescription'>
+                        Desc: Lorem ipsum odor amet, consectetuer adipiscing elit. Ante posuere ac faucibus quis consequat vel etiam. Pharetra per mauris ac lacinia ridiculus himenaeos euismod. Donec et nisi venenatis quis curae fames. Per risus ac malesuada amet vel ultrices morbi nunc malesuada. Fringilla hendrerit faucibus ante ullamcorper; gravida congue.
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Current;
