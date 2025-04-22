f487e3fee6908ed68deaa58711f94c82d6843951
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2025-04-22 17:00:40 +0100

    adjusted projects tile, added 3rd project insert

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
 import "./Projects.css"
 
 function Projects() {
 
     return (
-        <>
+            <>
             <div className='projectsTitle'>
                 Projects
             </div>
             <div className='projectsSection'>
                 <div className='projectContent'>
                     <div className='projectCardFirst'>
-                        <div className="projectCarda">
+                        <div className="projectCardA">
                             <div className='projectName'> EdScore </div>
-                            <div className='projectTech'> This is going to be my tech stack </div>
+                            <div className='projectTech'> PHP (Laravel), MySQL, TailwindCSS </div>
                             <div className='projectDescription'> Lorem ipsum odor amet, consectetuer adipiscing elit. Ante posuere ac faucibus quis consequat vel etiam. Pharetra per mauris ac lacinia ridiculus himenaeos euismod. Donec et nisi venenatis quis curae fames. Per risus ac malesuada amet vel ultrices morbi nunc malesuada. Fringilla hendrerit faucibus ante ullamcorper; gravida congue. </div>
                         </div>
-                        <div className='projectCar1b'>
+                        <div className='projectCardb'>
 
                         </div>
                     </div>
@@ -25,15 +24,25 @@ function Projects() {
                         <div className='projectCardb'>
 
                         </div>
-                        <div className='projectCarda'>
+                        <div className='projectCardA'>
                             <div className='projectName'> Portfolio V1</div>
                             <div className='projectTech'> This is going to be my tech stack </div>
                             <div className='projectDescription'> Lorem ipsum odor amet, consectetuer adipiscing elit. Ante posuere ac faucibus quis consequat vel etiam. Pharetra per mauris ac lacinia ridiculus himenaeos euismod. Donec et nisi venenatis quis curae fames. Per risus ac malesuada amet vel ultrices morbi nunc malesuada. Fringilla hendrerit faucibus ante ullamcorper; gravida congue. </div>
                         </div>
                     </div>
+                    <div className='projectCard'>
+                        <div className="projectCardA">
+                            <div className='projectName'> Hybrid Cryptosystem </div>
+                            <div className='projectTech'> Flask, Mongodb </div>
+                            <div className='projectDescription'> Lorem ipsum odor amet, consectetuer adipiscing elit. Ante posuere ac faucibus quis consequat vel etiam. Pharetra per mauris ac lacinia ridiculus himenaeos euismod. Donec et nisi venenatis quis curae fames. Per risus ac malesuada amet vel ultrices morbi nunc malesuada. Fringilla hendrerit faucibus ante ullamcorper; gravida congue. </div>
+                        </div>
+                        <div className='projectCardb'>
+
+                        </div>
+                    </div>
                 </div>
             </div>
-        </>
+            </>
     );
 }
 
